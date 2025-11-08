a6f512ad3b019855ce09b6c8845f41e05d844c14
张智伟 <372361363@qq.com>
2025-11-08 19:07:51 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,30 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    server: {
-        proxy: {
-            // 天聚数行API代理（如果需要通过代理访问）
-            // '/api/tianapi': {
-            //     target: 'https://apis.tianapi.com',
-            //     changeOrigin: true,
-            //     rewrite: (path) => path.replace(/^\/api\/tianapi/, ''),
-            //     configure: (proxy, _options) => {
-            //         proxy.on('error', (err, _req, _res) => {
+    base: './',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets'
+      }
+    // server: {
+    //     proxy: {
+    //         // 天聚数行API代理（如果需要通过代理访问）
+    //         // '/api/tianapi': {
+    //         //     target: 'https://apis.tianapi.com',
+    //         //     changeOrigin: true,
+    //         //     rewrite: (path) => path.replace(/^\/api\/tianapi/, ''),
+    //         //     configure: (proxy, _options) => {
+    //         //         proxy.on('error', (err, _req, _res) => {
                        
-            //         });
-            //         proxy.on('proxyReq', (proxyReq, req, _res) => {
+    //         //         });
+    //         //         proxy.on('proxyReq', (proxyReq, req, _res) => {
                        
-            //         });
-            //         proxy.on('proxyRes', (proxyRes, req, _res) => {
+    //         //         });
+    //         //         proxy.on('proxyRes', (proxyRes, req, _res) => {
                         
-            //         });
-            //     },
-            // }
-        }
-    }
+    //         //         });
+    //         //     },
+    //         // }
+    //     }
+    // }
 });
